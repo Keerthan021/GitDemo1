6ba155bc50b2c67f8528e9962f7e53ecfdda4ad5
Keerthan <keerthanderla@gmail.com>
2025-06-17 11:30:29 +0530

    import class updated

--- importClass.py
+++ importClass.py
@@ -2,6 +2,12 @@ from inheritance import Child
 
 obj2 = Child(124, "Steve Rogers", "steaverogers@gmail.com", "8896478230", "LA, USA")
 obj2.display()
+
 obj2.printData()
 
+# exra
+
+obj2.printData()
+obj2.printData()
+obj2.printData()
 obj2.printAll
